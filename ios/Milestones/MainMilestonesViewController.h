4e7ce85d5606b1889dd734ed007b28a385f97bbc
Nathan Pahucki <npahucki@gmail.com>
2015-03-26 16:54:26 -0300

    Add message and link on milestone page to DataParenting.

---
+++ ios/Milestones/MainMilestonesViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainMilestonesViewController.h
+//  EssentialTips
+//
+//  Created by Nathan  Pahucki on 3/26/15.
+//  Copyright (c) 2015 Infant IQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMilestonesViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *textLabel;
+@property (weak, nonatomic) IBOutlet UIButton *getTheAppButton;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *arrowBottomContraint;
+
+@end
---
+++ ios/Milestones/MainMilestonesViewController.m
@@ -0,0 +1,42 @@
+//
+//  MainMilestonesViewController.m
+//  EssentialTips
+//
+//  Created by Nathan  Pahucki on 3/26/15.
+//  Copyright (c) 2015 Infant IQ. All rights reserved.
+//
+
+#import "MainMilestonesViewController.h"
+
+@interface MainMilestonesViewController ()
+
+@end
+
+@implementation MainMilestonesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.textLabel.font = [UIFont fontForAppWithType:Medium andSize:32.0];
+    self.textLabel.textColor = [UIColor appGreyTextColor];
+    self.textLabel.text = [NSString stringWithFormat:self.textLabel.text, [Baby currentBaby].name, [Baby currentBaby].isMale ? @"him" : @"her"];
+    self.getTheAppButton.titleLabel.font = [UIFont fontForAppWithType:Book andSize:23.0];
+    [self.getTheAppButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+}
+
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self.view.layer removeAllAnimations];
+    self.arrowBottomContraint.constant = 32;
+    [self.view layoutIfNeeded];
+    [UIView animateWithDuration:3.0 delay:0.0 usingSpringWithDamping:0.1 initialSpringVelocity:0.0 options:UIViewAnimationOptionCurveEaseOut animations:^{
+        self.arrowBottomContraint.constant = 8;
+        [self.view layoutIfNeeded];
+    }                completion:NULL];
+}
+
+- (IBAction)didClickGetAppNowButton:(id)sender {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"itms://itunes.apple.com/us/app/dataparenting-baby-milestones/id905124835?mt=8"]];
+    [UsageAnalytics trackClickedToViewOtherAppInAppStore:@"DataParenting"];
+}
+
+@end
